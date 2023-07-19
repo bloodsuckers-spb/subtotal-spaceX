8a77ee444a7dd553a1195244baf96ccd09cda506
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-07-20 02:38:46 +0300

    feat: add fadein animation

--- src/shared/ui/img-media-card/ImgMediaCard.tsx
+++ src/shared/ui/img-media-card/ImgMediaCard.tsx
@@ -3,6 +3,8 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
+import styles from './ImgMediaCard.module.scss';
+
 type Props = {
   title: string;
   details: string;
@@ -11,28 +13,30 @@ type Props = {
 
 export const ImgMediaCard = ({ title, details, imageUrl }: Props) => {
   return (
-    <Card sx={{ maxWidth: 300 }}>
-      <CardMedia
-        component="img"
-        alt=""
-        height="140"
-        image={imageUrl}
-      />
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-        >
-          {title}
-        </Typography>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-        >
-          {details}
-        </Typography>
-      </CardContent>
-    </Card>
+    <div className={styles.card}>
+      <Card sx={{ maxWidth: 300 }}>
+        <CardMedia
+          component="img"
+          alt=""
+          height="140"
+          image={imageUrl}
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+          >
+            {title}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+          >
+            {details}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
