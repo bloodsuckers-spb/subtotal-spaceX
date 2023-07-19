b3dae5858ebc319bf55ae0d597bd8b5d6775bb75
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-07-20 00:50:02 +0300

    feat: add svg sprite

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -3,6 +3,9 @@ import styles from './Header.module.scss';
 export const Header = () => {
   return (
     <header className={styles.header}>
+      <svg className={styles.icon}>
+        <use href="sprite.svg#SpaceXLogo"></use>
+      </svg>
     </header>
   );
 };
